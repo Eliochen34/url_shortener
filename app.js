72c49ca2c1338fb212fbaece01f5576be71feb23
Elio <eliochen34@gmail.com>
2022-10-16 14:18:55 +0800

    Initialized Project

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+
+const routes = require('./routes')
+
+const app = express()
