bf2712b956fe9733d7d6ed1e860f0ed2a982fdb8
Elio <eliochen34@gmail.com>
2022-10-17 13:56:12 +0800

    feat: add shortURL creat

--- routes/modules/urls.js
+++ routes/modules/urls.js
@@ -6,10 +6,26 @@ const Url = require('../../models/url')
 
 router.post('/', (req, res) => {
   const url = req.body.url
-  console.log(req.body.url)
-  return Url.create({ originalUrl: url, stringForNew: 'tytyt' })
-    .then(url => res.render('result', {url}))
-    .catch(err => console.log(err))
+  if (!url) return res.redirect('/')
+  // 比對req.body.url是否存在資料庫
+  // 有在資料庫，顯示result
+  // 若沒有在資料庫，則至資料庫新增一筆資料
+  Url.findOne({ originalUrl: url})
+    .then(data =>
+       data ? data : Url.create({ originalUrl: url, stringForNew: '54312' })
+    )
+    .then(data =>
+      // 拿既有資料or新產生的資料去渲染result畫面
+      res.render('result', { stringForNew: data.stringForNew })
+    )
+    .catch(err => console.log(err)) 
+})
+
+router.get('/:stringForNew', (req, res) => {
+  const stringForNew = req.params.stringForNew
+  console.log('ok')
+  Url.find({stringForNew})
+    .then(data => res.redirect(data.originalUrl))
 })
 
 // router.get('/:id', (req, res) => {
