1bcf99bd9c3a1442661d4549b1a1358bda326c39
Elio <eliochen34@gmail.com>
2022-10-20 13:15:49 +0800

    rebase

--- routes/modules/urls.js
+++ routes/modules/urls.js
@@ -7,18 +7,19 @@ router.post('/', (req, res) => {
   const url = req.body.url
   if (!url) return res.redirect('/')
   // 比對req.body.url是否存在資料庫
-  // 有在資料庫，顯示result
+  // 有在資料庫，顯示urls
   // 若沒有在資料庫，則至資料庫新增一筆資料
-  Url.findOne({ originalUrl: url })
-    .then(data =>
+  return Url.findOne({ originalUrl: url})
+    .then(data =>{
       data ? data : Url.create({ originalUrl: url, stringForNew: createRandomText(5) })
-    )
-    .then(data => {
-      // 拿既有資料or新產生的資料去渲染result畫面
-      res.render('result', { origin: req.headers.origin, stringForNew: data.stringForNew })
-    }
-    )
-    .catch(err => console.log(err))
+      return res.render('urls', { origin: req.headers.origin, stringForNew: data.stringForNew })
+    })
+    // .then(data => {
+    //   // 拿既有資料or新產生的資料去渲染result畫面
+    //   return res.render('result', { origin: req.headers.origin, stringForNew: data.stringForNew })
+    //   }
+    // )
+    .catch(err => console.log(err)) 
 })
 
 router.get('/:stringForNew', (req, res) => {
